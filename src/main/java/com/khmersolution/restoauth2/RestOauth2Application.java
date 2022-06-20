dc99bee5f452b8949fc3e12e2306559226069eb6
ravuthz <ravuthz@gmail.com>
2022-06-20 13:25:46 +0700

    Init Spring Boot project from https://start.spring.io/

---
+++ src/main/java/com/khmersolution/restoauth2/RestOauth2Application.java
@@ -0,0 +1,13 @@
+package com.khmersolution.restoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/khmersolution/restoauth2/RestOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khmersolution.restoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
